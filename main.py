e20fcc1b22982f5c542ee517ea7dd654c7eb8141
adamson691-sketch <adamson691@wp.pl>
2025-09-05 08:47:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from keep_alive import keep_alive  # serwer do podtrzymania na Render
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+print("DEBUG TOKEN:", TOKEN[:10], "..." if TOKEN else "NONE")
+
 CHANNEL_ID_RAW = os.getenv("CHANNEL_ID")
 
 if not TOKEN:
