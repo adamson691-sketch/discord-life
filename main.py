d962ff279673d2060a823e029d474c3ef0a214ed
adamson691-sketch <adamson691@wp.pl>
2025-09-25 12:09:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -377,7 +377,7 @@ async def schedule_memes():
     await bot.wait_until_ready()
     while not bot.is_closed():
         now = datetime.now(tz)
-        targets = [(12, 0), (21, 37)]
+        targets = [(12, 15), (21, 37)]
         next_time = None
 
         for hour, minute in targets:
